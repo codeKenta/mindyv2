81879e9fa788775af479e687773ab55245d34399
Kenneth Österholm <kenneth.osterholm@makingwaves.com>
2021-04-25 21:10:23 +0200

    first commit: init strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dcf1503a8189254e96f33f553f96b29d'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '972e9915-5f7f-41d0-8b98-568059350860'
+};
